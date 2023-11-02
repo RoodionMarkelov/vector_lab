2d0348cfaf622994c9a1812d7cf264ecd28efaed
rodio <rodionmarkelov01229@gmail.com>
2023-11-02 20:20:11 +0400

    Добавьте файлы проекта.

---
+++ vector_lab/vector_lab.cpp
@@ -0,0 +1,14 @@
+﻿// vector_lab.cpp: определяет точку входа для приложения.
+//
+
+#include "vector_lab.h"
+
+using namespace std;
+using namespace lab;
+
+int main()
+{
+	Vector<int> a;
+	cout << a.get_size() << endl;
+	return 0;
+}
---
+++ vector_lab/vector_lab.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include <iostream>
+
+namespace lab {
+	const float EPSILON = 0.00001f;
+
+	template<typename T>
+	class Vector {
+		T* _data;
+		int _size;
+	public:
+		Vector() : _data(nullptr), _size(0) {}
+
+		Vector(int size, T* vector) {
+			_size = size;
+			_vect = new T[size];
+			for (int i = 0; i < size; ++i) {
+				_vect[i] = vector[i];
+			}
+		}
+
+		Vector(int size) {
+			_vect = new T[size];
+			for (int i = 0; i < size; ++i) {
+				_vect[i] = 0;
+			}
+			_size = size;
+		}
+
+		int get_size() const {
+			return _size;
+		}
+
+	};
+}
