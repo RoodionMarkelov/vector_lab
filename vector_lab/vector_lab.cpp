beee7bca2c5856a8089e6c1c3b921d210fb67b4b
rodio <rodionmarkelov01229@gmail.com>
2023-11-02 21:07:34 +0400

    ctor+some_operator

--- vector_lab/vector_lab.cpp
+++ vector_lab/vector_lab.cpp
@@ -1,14 +1,59 @@
 ﻿// vector_lab.cpp: определяет точку входа для приложения.
 //
-
 #include "vector_lab.h"
 
 using namespace std;
-using namespace lab;
 
 int main()
 {
 	Vector<int> a;
-	cout << a.get_size() << endl;
+	Vector<float> b(2);
+	Vector<float> c(b);
+
+	double a1[] = { 3.0, 4.0, 0.7 };
+	Vector<double> d1(3, a1);
+
+	int a2[] = { 3, 4, 0 };
+	Vector<int> d2(3, a2);
+
+	float a3[] = { 3.0f, 4.0f, 0.5f };
+	Vector<float> d3(3, a3);
+
+	//=====================================
+	//Constructor + .abs()
+	//=====================================
+
+	std::cout << a.get_size() << endl;
+	std::cout << b.get_size() << endl;
+	std::cout << c.get_size() << endl;
+	std::cout << d1.get_size() << endl;
+	std::cout << "Vector: " << b << endl;
+	std::cout << "Vector: " << d1 << endl;
+	std::cout << "Vector: " << d2 << endl;
+	std::cout << "Vector: " << d3 << endl;
+	std::cout << "abs: " << d1.abs() << endl;
+	std::cout << "abs: " << d2.abs() << endl;
+	std::cout << "abs: " << d3.abs() << endl;
+
+	//=======================================
+	//Operator
+	//=======================================
+
+	Vector<double> v1 = d1 + d1;
+	Vector<int> v2 = d2 + d2;
+	Vector<float> v3 = d3 + d3;
+
+	std::cout << "Vector: " << v1 << endl;
+	std::cout << "Vector: " << v2 << endl;
+	std::cout << "Vector: " << v3 << endl;
+
+	Vector<double> v11 = d1 - d1;
+	Vector<int> v22 = d2 - d2;
+	Vector<float> v33 = d3 - d3;
+
+	std::cout << "Vector: " << v11 << endl;
+	std::cout << "Vector: " << v22 << endl;
+	std::cout << "Vector: " << v33 << endl;
+
 	return 0;
 }
--- vector_lab/vector_lab.h
+++ vector_lab/vector_lab.h
@@ -1,36 +1,121 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <stdexcept>
 
-namespace lab {
-	const float EPSILON = 0.00001f;
+const float EPSILON = 0.00001f;
+
+template<typename T>
+class Vector {
+	T* _data;
+	int _size;
+public:
+	Vector() : _data(nullptr), _size(0) {}
+
+	Vector(int size, T* vector) {
+		_size = size;
+		_data = new T[size];
+		for (int i = 0; i < size; ++i) {
+			_data[i] = vector[i];
+		}
+	}
+
+	Vector(int size) {
+		_data = new T[size];
+		for (int i = 0; i < size; ++i) {
+			_data[i] = 0;
+		}
+		_size = size;
+	}
+
+	int get_size() const {
+		return _size;
+	}
+
+	Vector(const Vector<T>& other) :
+		_data(new T[other._size]),
+		_size(other._size)
+	{
+		for (int i = 0; i < _size; ++i) {
+			_data[i] = other._data[i];
+		}
+	}
+
+	T& operator[](const int index) const{
+		/*if (index < 0 || _size <= index) {
+			throw out_of_range("[Vector::operator[]] Index is out of range.");
+		}*/
+		return _data[index];
+	}
+
+	Vector<T> operator=(const Vector<T>& rhs) {
+		/*if (rhs.get_size() != _size) {
+			throw runtime_error("[Vector::operator=] Different size.");
+		}*/
+		Vector<T> copy(rhs);
+		swap(copy);
+		return *this;
+	}
+
+	void swap(Vector<T> other) {
+		std::swap(_data, other._data);
+		std::swap(_size, other._size);
+	}
+
+	double abs() {
+		double value = 0;
+		for (int i = 0; i < _size; ++i) {
+			value += pow(double(_data[i]), 2);
+		}
+		return sqrt(value);
+	}
+
+	friend std::ostream& operator<<(std::ostream& stream, Vector& lhs) {
+		Vector<T> temp(lhs);
+		stream << "(";
+		for (int i = 0; i < temp.get_size(); ++i) {
+			if (i == temp.get_size() - 1) {
+				stream << temp._data[i] << ")";
+				continue;
+			}
+			stream << temp._data[i] << ",";
+		}
+
+		return stream;
+	}
+
+	~Vector() {
+		delete[] _data;
+
+		_data = nullptr;
+		_size = 0;
+	}
+
 
 	template<typename T>
-	class Vector {
-		T* _data;
-		int _size;
-	public:
-		Vector() : _data(nullptr), _size(0) {}
-
-		Vector(int size, T* vector) {
-			_size = size;
-			_vect = new T[size];
-			for (int i = 0; i < size; ++i) {
-				_vect[i] = vector[i];
-			}
+	Vector<T> operator+(const Vector<T>& rhs) {
+		/*if (lhs.get_size() != rhs.get_size()) {
+			throw runtime_error("[Vector::operator+] Different size.");
+		}*/
+		int size = rhs.get_size();
+		Vector<T> temp(*this);
+		for (int i = 0; i < temp.get_size(); ++i) {
+			temp[i] = _data[i] + rhs._data[i];
 		}
+		return temp;
+	}
 
-		Vector(int size) {
-			_vect = new T[size];
-			for (int i = 0; i < size; ++i) {
-				_vect[i] = 0;
-			}
-			_size = size;
+	template<typename T>
+	Vector<T> operator-(const Vector<T>& rhs) {
+		/*if (lhs.get_size() != rhs.get_size()) {
+			throw runtime_error("[Vector::operator+] Different size.");
+		}*/
+		Vector<T> temp(*this);
+
+		for (int i = 0; i < temp.get_size(); ++i) {
+			temp[i] = _data[i] - rhs._data[i];
 		}
+		return temp;
+	}
 
-		int get_size() const {
-			return _size;
-		}
-
-	};
-}
+};
