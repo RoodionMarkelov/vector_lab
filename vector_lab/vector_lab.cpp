09d8e5e2ec2d1973de153ea15e4128423871936c
rodio <rodionmarkelov01229@gmail.com>
2023-11-07 18:41:43 +0400

    COMPLEX<T>

--- vector_lab/vector_lab.cpp
+++ vector_lab/vector_lab.cpp
@@ -1,126 +1,201 @@
-﻿// vector_lab.cpp: определяет точку входа для приложения.
-//
-#include "vector_lab.h"
+﻿#include "vector_lab.h"
 
 using namespace std;
 
 int main()
 {
-	Vector<int> a;
-	Vector<float> b(2);
-	Vector<float> c(b);
+	//Vector<int> a;
+	//Vector<float> b(2);
+	//Vector<float> c(b);
 
-	double a1[] = { 3.0, 4.0, 0.7 };
-	Vector<double> d1(3, a1);
+	//Vector<int> vec1007(5, 1, 100);
+	//std::cout << vec1007 << endl;
 
-	int a2[] = { 3, 4, 0 };
-	Vector<int> d2(3, a2);
+	//Vector<float> vec1007(4, 1.0f, 100.0f);
+	//std::cout << vec1007 << endl;
 
-	float a3[] = { 3.0f, 4.0f, 0.5f };
-	Vector<float> d3(3, a3);
+	//Vector<double> vec10007(7, 1.0, 100.0);
+	//std::cout << vec10007 << endl;
 
-	//=====================================
-	//Constructor + .abs()
-	//=====================================
+	//double a1[] = { 3.0, 4.0, 0.7 };
+	//Vector<double> d1(3, a1);
 
-	std::cout << a.get_size() << endl;
-	std::cout << b.get_size() << endl;
-	std::cout << c.get_size() << endl;
-	std::cout << d1.get_size() << endl;
-	std::cout << "Vector: " << b << endl;
-	std::cout << "Vector: " << d1 << endl;
-	std::cout << "Vector: " << d2 << endl;
-	std::cout << "Vector: " << d3 << endl;
-	std::cout << "abs: " << d1.abs() << endl;
-	std::cout << "abs: " << d2.abs() << endl;
-	std::cout << "abs: " << d3.abs() << endl;
+	//int a2[] = { 3, 4, 0 };
+	//Vector<int> d2(3, a2);
 
-	//=======================================
-	//Operator
-	//=======================================
+	//float a3[] = { 3.0f, 4.0f, 0.5f };
+	//Vector<float> d3(3, a3);
 
-	Vector<double> v1 = d1 + d1;
-	Vector<int> v2 = d2 + d2;
-	Vector<float> v3 = d3 + d3;
+	////=====================================
+	////Constructor + .abs()
+	////=====================================
+
+	//std::cout << a.get_size() << endl;
+	//std::cout << b.get_size() << endl;
+	//std::cout << c.get_size() << endl;
+	//std::cout << d1.get_size() << endl;
+	//std::cout << "Vector: " << b << endl;
+	//std::cout << "Vector: " << d1 << endl;
+	//std::cout << "Vector: " << d2 << endl;
+	//std::cout << "Vector: " << d3 << endl;
+	//std::cout << "abs: " << d1.abs() << endl;
+	//std::cout << "abs: " << d2.abs() << endl;
+	//std::cout << "abs: " << d3.abs() << endl;
+
+	////=======================================
+	////Operator
+	////=======================================
+
+	//Vector<double> v1 = d1 + d1;
+	//Vector<int> v2 = d2 + d2;
+	//Vector<float> v3 = d3 + d3;
+
+	//std::cout << "Operaror+: " << endl;
+	//std::cout << "Vector: " << v1 << endl;
+	//std::cout << "Vector: " << v2 << endl;
+	//std::cout << "Vector: " << v3 << endl;
+
+	//Vector<double> v11 = d1 - d1;
+	//Vector<int> v21 = d2 - d2;
+	//Vector<float> v31 = d3 - d3;
+
+	//std::cout << "Operaror-: " << endl;
+	//std::cout << "Vector: " << v11 << endl;
+	//std::cout << "Vector: " << v21 << endl;
+	//std::cout << "Vector: " << v31 << endl;
+
+	//Vector<double> v12 = d1 * 2.0;
+	//Vector<int> v22 = d2 * 2;
+	//Vector<float> v32 = d3 * 2.0f;
+	//std::cout << "Operaror* vector on val " << endl;
+	//std::cout << "Vector: " << v12 << endl;
+	//std::cout << "Vector: " << v22 << endl;
+	//std::cout << "Vector: " << v32 << endl;
+
+	//Vector<double> v13 = 2.0 * d1;
+	//Vector<int> v23 = 2 * d2;
+	//Vector<float> v33 = 2.0f * d3;
+
+	//std::cout << "Operaror* val on vector " << endl;
+	//std::cout << "Vector: " << v13 << endl;
+	//std::cout << "Vector: " << v23 << endl;
+	//std::cout << "Vector: " << v33 << endl;
+
+	//Vector<double> v14 = d1 / 2.0;
+	//Vector<int> v24 = d2 / 2;
+	//Vector<float> v34 = d3 / 2.0f;
+
+	//std::cout << "Operaror/ " << endl;
+	//std::cout << "Vector: " << v14 << endl;
+	//std::cout << "Vector: " << v24 << endl;
+	//std::cout << "Vector: " << v34 << endl;
+
+	//double a11[] = { 3.0, 4.0, 0.6 };
+	//Vector<double> d11(3, a11);
+
+	//int a22[] = { 3, 5, 0 };
+	//Vector<int> d22(3, a22);
+
+	//float a33[] = { 1.0f, 4.0f, 0.5f };
+	//Vector<float> d33(3, a33);
+
+	//std::cout << "Operaror bool: " << endl;
+	//std::cout << (d11 == d1) << endl;
+	//std::cout << (d1 == d1) << endl;
+	//std::cout << (d22 == d2) << endl;
+	//std::cout << (d2 == d2) << endl;
+	//std::cout << (d33 == d3) << endl;
+	//std::cout << (d3 == d3) << endl;
+	//std::cout << "Now reverse bool: " << endl;
+	//std::cout << (d11 != d1) << endl;
+	//std::cout << (d1 != d1) << endl;
+	//std::cout << (d22 != d2) << endl;
+	//std::cout << (d2 != d2) << endl;
+	//std::cout << (d33 != d3) << endl;
+	//std::cout << (d3 != d3) << endl;
+
+	////==============================
+	////find_norm
+	////==============================
+	//Vector<double> vec1 = find_norm(d1);
+	//Vector<double> vec2 = find_norm(d2);
+	//Vector<double> vec3 = find_norm(d3);
+
+	//std::cout << "Vector: " << d1 << endl;
+	//std::cout << "Vector: " << d2 << endl;
+	//std::cout << "Vector: " << d3 << endl;
+	//std::cout << vec1 << endl;
+	//std::cout << vec2 << endl;
+	//std::cout << vec3 << endl;
+	
+	//==============================================
+	//Complex
+	//==============================================
+	complex<float> list1[] = {complex<float>(1.0f,0.0f), complex<float>(1.0f,1.0f), complex<float>(-1.0f,0.0f) };
+	Vector<complex<float>> vector1(3, list1);
+	std::cout << "Vector: " << vector1 << vector1.abs() << endl;
+
+	Vector<complex<float>> vector11(3, -10.0f, 10.0f);
+	std::cout << "Vector: " << vector11 << endl;
+
+	complex<double> list2[] = { complex<double>(1.0,2.5), complex<double>(1.0,2.0), complex<double>(-1.0,1.27) };
+	Vector<complex<double>> vector2(3, list2);
+	std::cout << "Vector: " << vector2 << endl;
+
+	Vector<complex<double>> vector22(3, -10.0, 10.0);
+	std::cout << "Vector: " << vector22 << endl;
+
+	Vector<complex<float>> sum1 = (vector1 + vector11);
+	Vector<complex<double>> sum2 = (vector2 + vector22);
 
 	std::cout << "Operaror+: " << endl;
-	std::cout << "Vector: " << v1 << endl;
-	std::cout << "Vector: " << v2 << endl;
-	std::cout << "Vector: " << v3 << endl;
+	std::cout << "Vector: " << sum1 << endl;
+	std::cout << "Vector: " << sum2 << endl;
 
-	Vector<double> v11 = d1 - d1;
-	Vector<int> v21 = d2 - d2;
-	Vector<float> v31 = d3 - d3;
-
+	Vector<complex<float>> minus1 = (vector1 - vector1);
+	Vector<complex<double>> minus2 = (vector2 - vector2);
 	std::cout << "Operaror-: " << endl;
-	std::cout << "Vector: " << v11 << endl;
-	std::cout << "Vector: " << v21 << endl;
-	std::cout << "Vector: " << v31 << endl;
+	std::cout << "Vector: " << minus1 << endl;
+	std::cout << "Vector: " << minus2 << endl;
 
-	Vector<double> v12 = d1 * 2.0;
-	Vector<int> v22 = d2 * 2;
-	Vector<float> v32 = d3 * 2.0f;
-	std::cout << "Operaror* vector on val " << endl;
-	std::cout << "Vector: " << v12 << endl;
-	std::cout << "Vector: " << v22 << endl;
-	std::cout << "Vector: " << v32 << endl;
 
-	Vector<double> v13 = 2.0 * d1;
-	Vector<int> v23 = 2 * d2;
-	Vector<float> v33 = 2.0f * d3;
+	complex<float> mult1 = (vector1 * vector1);
+	complex<double> mult2 = (vector2 * vector2);
+	std::cout << "Operaror* vec on vec: " << endl;
+	std::cout << "Vector: " << mult1 << endl;
+	std::cout << "Vector: " << mult2 << endl;
 
-	std::cout << "Operaror* val on vector " << endl;
-	std::cout << "Vector: " << v13 << endl;
-	std::cout << "Vector: " << v23 << endl;
-	std::cout << "Vector: " << v33 << endl;
+	Vector<complex<float>> mult3 = (vector1 * complex<float>(2.0f,0.0f));
+	Vector<complex<double>> mult4 = (vector2 * complex<double>(0.0, 1.0));
+	std::cout << "Operaror* vec on val: " << endl;
+	std::cout << "Vector: " << mult3 << endl;
+	std::cout << "Vector: " << mult4 << endl;
 
-	Vector<double> v14 = d1 / 2.0;
-	Vector<int> v24 = d2 / 2;
-	Vector<float> v34 = d3 / 2.0f;
+	Vector<complex<float>> mult5 = (complex<float>(2.0f, 0.0f) * vector1);
+	Vector<complex<double>> mult6 = (complex<double>(0.0, 1.0) * vector2);
+	std::cout << "Operaror* val on vec: " << endl;
+	std::cout << "Vector: " << mult5 << endl;
+	std::cout << "Vector: " << mult6 << endl;
 
-	std::cout << "Operaror/ " << endl;
-	std::cout << "Vector: " << v14 << endl;
-	std::cout << "Vector: " << v24 << endl;
-	std::cout << "Vector: " << v34 << endl;
-
-	double a11[] = { 3.0, 4.0, 0.6 };
-	Vector<double> d11(3, a11);
-
-	int a22[] = { 3, 5, 0 };
-	Vector<int> d22(3, a22);
-
-	float a33[] = { 1.0f, 4.0f, 0.5f };
-	Vector<float> d33(3, a33);
-
+	Vector<complex<float>> del1 = (vector1 / complex<float>(2.0f, 0.0f));
+	Vector<complex<double>> del2 = (vector2 / complex<double>(0.0, 2.0));
+	std::cout << "Operaror/ vec on val: " << endl;
+	std::cout << "Vector: " << del1 << endl;
+	std::cout << "Vector: " << del2 << endl;
+	
 	std::cout << "Operaror bool: " << endl;
-	std::cout << (d11 == d1) << endl;
-	std::cout << (d1 == d1) << endl;
-	std::cout << (d22 == d2) << endl;
-	std::cout << (d2 == d2) << endl;
-	std::cout << (d33 == d3) << endl;
-	std::cout << (d3 == d3) << endl;
+	std::cout << (vector1 == vector1) << endl;
+	std::cout << (vector11 == vector1) << endl;
+	std::cout << (vector22 == vector2) << endl;
+	std::cout << (vector2 == vector2) << endl;
+	
 	std::cout << "Now reverse bool: " << endl;
-	std::cout << (d11 != d1) << endl;
-	std::cout << (d1 != d1) << endl;
-	std::cout << (d22 != d2) << endl;
-	std::cout << (d2 != d2) << endl;
-	std::cout << (d33 != d3) << endl;
-	std::cout << (d3 != d3) << endl;
-
-	//==============================
-	//find_norm
-	//==============================
-	Vector<double> vec1 = find_norm(d1);
-	Vector<double> vec2 = find_norm(d2);
-	Vector<double> vec3 = find_norm(d3);
-
-	std::cout << "Vector: " << d1 << endl;
-	std::cout << "Vector: " << d2 << endl;
-	std::cout << "Vector: " << d3 << endl;
-	std::cout << vec1 << endl;
-	std::cout << vec2 << endl;
-	std::cout << vec3 << endl;
+	std::cout << (vector1 != vector1) << endl;
+	std::cout << (vector11 != vector1) << endl;
+	std::cout << (vector22 != vector2) << endl;
+	std::cout << (vector2 != vector2) << endl;
 	
 
+	
 	return 0;
 }
--- vector_lab/vector_lab.h
+++ vector_lab/vector_lab.h
@@ -2,7 +2,11 @@
 
 #include <cmath>
 #include <iostream>
+#include <random>
+#include <complex>
 #include <stdexcept>
+
+using namespace std;
 
 const float EPSILON = 0.00001f;
 
@@ -14,6 +18,9 @@ public:
 	Vector() : _data(nullptr), _size(0) {}
 
 	Vector(int size, T* vector) {
+		if (size <= 0) {
+			throw out_of_range("Size of vector cannot be negative or zero");
+		}
 		_size = size;
 		_data = new T[size];
 		for (int i = 0; i < size; ++i) {
@@ -21,7 +28,21 @@ public:
 		}
 	}
 
+	Vector(const int size, const T& min_value, const T& max_value) {
+		if (size <= 0) {
+			throw out_of_range("Size of vector cannot be negative or zero");
+		}
+		_size = size;
+		_data = new T[size];
+		for (int i(0); i < size; ++i) {
+			_data[i] = (T)((double)min_value + (double)(rand()) / RAND_MAX * (max_value - min_value));
+		}
+	}
+
 	Vector(int size) {
+		if (size <= 0) {
+			throw out_of_range("Size of vector cannot be negative or zero");
+		}
 		_data = new T[size];
 		for (int i = 0; i < size; ++i) {
 			_data[i] = 0;
@@ -43,16 +64,16 @@ public:
 	}
 
 	T& operator[](const int index) const {
-		/*if (index < 0 || _size <= index) {
-			throw out_of_range("[Vector::operator[]] Index is out of range.");
-		}*/
+		if (index < 0 || index >= get_size()) {
+			throw out_of_range("[Vector::operator[]] Index out of size");
+		}
 		return _data[index];
 	}
 
 	Vector<T> operator=(const Vector<T>& rhs) {
-		/*if (rhs.get_size() != _size) {
+		if (rhs.get_size() != _size) {
 			throw runtime_error("[Vector::operator=] Different size.");
-		}*/
+		}
 		Vector<T> copy(rhs);
 		swap(copy);
 		return *this;
@@ -71,7 +92,7 @@ public:
 		return sqrt(value);
 	}
 
-	friend std::ostream& operator<<(std::ostream& stream, Vector& lhs) {
+	friend std::ostream& operator<<(std::ostream& stream, Vector<T>& lhs) {
 		Vector<T> temp(lhs);
 		stream << "(";
 		for (int i = 0; i < temp.get_size(); ++i) {
@@ -95,9 +116,9 @@ public:
 
 	template<typename T>
 	Vector<T> operator+(const Vector<T>& rhs) {
-		/*if (lhs.get_size() != rhs.get_size()) {
+		if (rhs.get_size() != rhs.get_size()) {
 			throw runtime_error("[Vector::operator+] Different size.");
-		}*/
+		}
 		int size = rhs.get_size();
 		Vector<T> temp(*this);
 		for (int i = 0; i < temp.get_size(); ++i) {
@@ -108,9 +129,9 @@ public:
 
 	template<typename T>
 	Vector<T> operator-(const Vector<T>& rhs) {
-		/*if (lhs.get_size() != rhs.get_size()) {
+		if (rhs.get_size() != rhs.get_size()) {
 			throw runtime_error("[Vector::operator+] Different size.");
-		}*/
+		}
 		Vector<T> temp(*this);
 
 		for (int i = 0; i < temp.get_size(); ++i) {
@@ -121,9 +142,9 @@ public:
 
 	template<typename T>
 	T operator*(const Vector<T>& rhs) {
-		/*if (lhs.get_size() != rhs.get_size()) {
+		if (rhs.get_size() != rhs.get_size()) {
 			throw runtime_error("[Vector::operator+] Different size.");
-		}*/
+		}
 		T value = 0;
 
 		for (int i = 0; i < rhs.get_size(); ++i) {
@@ -187,7 +208,7 @@ template<typename T>
 		}
 		Vector<T> temp(other);
 		Vector<double> norm(other.get_size());
-		norm.get_element(0) = double((-1) * temp.get_element(1));
+		norm.get_element(0) = double(((-1) * temp.get_element(1)) / temp.get_element(0));
 		norm.get_element(1) = 1.0;
 		for (int i = 2; i < norm.get_size(); ++i) {
 			norm.get_element(i) = 0.0;
@@ -199,6 +220,219 @@ template<typename T>
 		return norm;
 	}
 
+	//=============================================================================
+	//=============================================================================
+	//=============================================================================
 
+	template<typename T>
+	class Vector<complex<T>> {
+		complex<T>* _data;
+		int _size;
+	public:
+		Vector() : _data(nullptr), _size(0) {}
+
+		Vector(int size, complex<T>* vector) {
+			if (size <= 0) {
+				throw out_of_range("Size of vector cannot be negative or zero");
+			}
+			_size = size;
+			_data = new complex<T>[size];
+			for (int i = 0; i < size; ++i) {
+				_data[i] = vector[i];
+			}
+		}
+
+		Vector(const int size, const T& min_value, const T& max_value) {
+			if (size <= 0) {
+				throw out_of_range("Size of vector cannot be negative or zero");
+			}
+			_size = size;
+			_data = new complex<T>[size];
+			for (int i(0); i < size; ++i) {
+				T re = min_value + (T)(rand()) / RAND_MAX * (max_value - min_value);
+				T im = min_value + (T)(rand()) / RAND_MAX * (max_value - min_value);
+				_data[i] = complex<T>(re, im);
+			}
+		}
+
+		Vector(int size) {
+			if (size <= 0) {
+				throw out_of_range("Size of vector cannot be negative or zero");
+			}
+			_data = new complex<T>[size];
+			for (int i = 0; i < size; ++i) {
+				_data[i] = complex<T>(0,0);
+			}
+			_size = size;
+		}
+
+		int get_size() const {
+			return _size;
+		}
+
+		Vector(const Vector<complex<T>>& other) :
+			_data(new complex<T>[other._size]),
+			_size(other._size)
+		{
+			for (int i = 0; i < _size; ++i) {
+				_data[i] = other._data[i];
+			}
+		}
+
+		complex<T>& operator[](const int index) const {
+			if (index < 0 || index >= get_size()) {
+				throw out_of_range("[Vector::operator[]] Index out of size");
+			}
+			return _data[index];
+		}
+
+		Vector<complex<T>> operator=(const Vector<complex<T>>& rhs) {
+			if (rhs.get_size() != _size) {
+				throw runtime_error("[Vector::operator=] Different size.");
+			}
+			Vector<complex<T>> copy(rhs);
+			swap(copy);
+			return *this;
+		}
+
+		void swap(Vector<complex<T>> other) {
+			std::swap(_data, other._data);
+			std::swap(_size, other._size);
+		}
+
+		double abs() {
+			double value = 0;
+			for (int i = 0; i < _size; ++i) {
+				value += std::abs(_data[i]);
+
+			}
+			return sqrt(value);
+		}
+
+		friend std::ostream& operator<<(std::ostream& stream, Vector<complex<T>>& lhs) {
+			Vector<complex<T>> temp(lhs);
+			stream << "(";
+			for (int i = 0; i < temp.get_size(); ++i) {
+				if (i == temp.get_size() - 1) {
+					stream << temp._data[i] << ")";
+					continue;
+				}
+				stream << temp._data[i] << ",";
+			}
+
+			return stream;
+		}
+
+		~Vector() {
+			delete[] _data;
+
+			_data = nullptr;
+			_size = 0;
+		}
+
+		Vector<complex<T>> operator+(const Vector<complex<T>>& rhs) {
+			if (rhs.get_size() != rhs.get_size()) {
+				throw runtime_error("[Vector::operator+] Different size.");
+			}
+			int size = rhs.get_size();
+			Vector<complex<T>> temp(*this);
+			for (int i = 0; i < temp.get_size(); ++i) {
+				temp[i] = _data[i] + rhs._data[i];
+			}
+			return temp;
+		}
+
+		template<typename T>
+		Vector<complex<T>> operator-(const Vector<complex<T>>& rhs) {
+			if (rhs.get_size() != rhs.get_size()) {
+				throw runtime_error("[Vector::operator+] Different size.");
+			}
+			Vector<complex<T>> temp(*this);
+
+			for (int i = 0; i < temp.get_size(); ++i) {
+				temp[i] = _data[i] - rhs._data[i];
+			}
+			return temp;
+		}
+
+		template<typename T>
+		complex<T> operator*(const Vector<complex<T>>& rhs) {
+			if (rhs.get_size() != rhs.get_size()) {
+				throw runtime_error("[Vector::operator+] Different size.");
+			}
+			complex<T> value = complex<T>(0, 0);
+
+			for (int i = 0; i < rhs.get_size(); ++i) {
+				value += (_data[i] * complex<T>(rhs._data[i].real(), rhs._data[i].imag() * (-1)));
+			}
+			return value;
+		}
+
+		template<typename T>
+		Vector<complex<T>> operator*(const complex<T> value) {
+			Vector<complex<T>> temp(*this);
+			for (int i = 0; i < temp.get_size(); ++i) {
+				temp._data[i] = value * _data[i];
+			}
+			return temp;
+		}
+
+		template<typename T>
+		Vector<complex<T>> operator/(const complex<T> value) {
+			Vector<complex<T>> temp(*this);
+			for (int i = 0; i < temp.get_size(); ++i) {
+				temp._data[i] = _data[i] / value;
+			}
+			return temp;
+		}
+		template<typename T>
+		bool operator==(const Vector<complex<T>>& rhs) {
+			if (get_size() == rhs.get_size()) {
+				for (int i = 0; i < get_size(); ++i) {
+					auto val = (_data[i] - rhs._data[i]);
+					if (std::abs(val) > EPSILON) {
+						return false;
+					}
+				}
+				return true;
+			}
+			return false;
+		}
+
+		template<typename T>
+		bool operator!=(const Vector<complex<T>>& rhs) {
+			return !(*this == rhs);
+		}
+
+		complex<T>& get_element(const int index) {
+			return _data[index];
+		}
+	};
+
+	template<typename T>
+	Vector<complex<T>> operator*(const complex<T> value, const Vector<complex<T>>& lhs) {
+		Vector<complex<T>> temp(lhs);
+		return temp * value;
+	}
+
+	template<typename T>
+	Vector<complex<double>> find_norm(const Vector<complex<T>> other) {
+		if (other.get_size() < 2) {
+			Vector<complex<double>> temp(1);
+			return temp;
+		}
+		Vector<T> temp(other);
+		Vector<double> norm(other.get_size());
+		norm.get_element(0) = complex<double>((-1) * temp.get_element(1));
+		norm.get_element(1) = 1.0;
+		for (int i = 2; i < norm.get_size(); ++i) {
+			norm.get_element(i) = 0.0;
+		}
+
+		for (int i = 0; i < norm.get_size(); ++i) {
+			norm.get_element(i) = norm.get_element(i) / norm.abs();
+		}
+		return norm;
+	}
 
 
